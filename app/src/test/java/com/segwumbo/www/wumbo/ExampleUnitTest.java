91f743c8979e3bee4a0f5a0f8aa4703bdb7117d1
AnthonyLaiRH <lai.anthony.rh@gmail.com>
2018-11-08 12:00:32 -0500

    First Unit Tests

--- app/src/test/java/com/segwumbo/www/wumbo/ExampleUnitTest.java
+++ app/src/test/java/com/segwumbo/www/wumbo/ExampleUnitTest.java
@@ -1,6 +1,13 @@
 package com.segwumbo.www.wumbo;
 
+import android.util.Log;
+import android.widget.TextView;
+
 import org.junit.Test;
+import org.junit.Before;
+
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +17,19 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private MainLoginActivity loginActivity;
+
+    @Before
+    public void setup(){
+        loginActivity = new MainLoginActivity();
+    }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void bothRight(){
+        TextView usernameField = loginActivity.findViewById(R.id.usernameEditText);
+        usernameField.setText("admin");
+        TextView passwordField = loginActivity.findViewById(R.id.passwordEditText);
+        passwordField.setText("admin");
+        assertTrue(loginActivity.validUser(loginActivity.findViewById(R.id.usernameEditText).toString(), loginActivity.findViewById(R.id.passwordEditText).toString()));
     }
 }
