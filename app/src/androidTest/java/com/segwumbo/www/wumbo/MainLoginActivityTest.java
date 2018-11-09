d1452b3b534a1124b84a3f4160ff67087f8d13db
apose046 <aj.poserio1@gmail.com>
2018-11-09 11:13:18 -0500

    JUnit Test 1 - Both U and P right

---
+++ app/src/androidTest/java/com/segwumbo/www/wumbo/MainLoginActivityTest.java
@@ -0,0 +1,58 @@
+package com.segwumbo.www.wumbo;
+
+import android.os.Bundle;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.TextView;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class MainLoginActivityTest {
+
+    //Check User name
+
+    @Rule
+    public ActivityTestRule<MainLoginActivity> mActivityTestRule= new ActivityTestRule<MainLoginActivity>(MainLoginActivity.class);
+    private MainLoginActivity mActivity=null;
+
+    private static ArrayList<UserAccount> allUserAccounts = new ArrayList<UserAccount>();
+
+    private TextView textUser;
+    private TextView textPassword;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+
+        UserAccount acc1 = new UserAccount("abcde/","hello.world@gmail.com","rufus","snoopy","sponge");
+        MainLoginActivity.allUserAccounts.add(acc1);
+
+    }
+
+        @Test
+        @UiThreadTest
+        public void testBothRight() throws Exception{
+        assertNotNull(mActivity.findViewById(R.id.usernameEditText));
+        textUser = mActivity.findViewById(R.id.usernameEditText);
+        textUser.setText("rufus");
+        String name = textUser.getText().toString();
+
+        assertNotNull(mActivity.findViewById(R.id.passwordEditText));
+        textPassword = mActivity.findViewById(R.id.passwordEditText);
+        textPassword.setText("snoopy");
+        String password = textPassword.getText().toString();
+
+        assertSame(mActivity.validUser(name,password),true);
+        }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
